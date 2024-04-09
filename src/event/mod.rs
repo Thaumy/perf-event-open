8aeb32c95c37d5b3daf6b5d52d5366c24554df4d
Thaumy <thaumy@outlook.com>
2024-04-09 12:21:33 +0800

    add `Event` type

---
+++ src/event/mod.rs
@@ -0,0 +1,33 @@
+#[derive(Clone, Debug)]
+pub struct Event(pub(super) EventConfig);
+
+#[derive(Clone, Debug)]
+pub(super) struct EventConfig {
+    pub ty: u32,
+    pub config: u64,
+    pub config1: u64,
+    pub config2: u64,
+    pub config3: u64,
+    pub bp_type: u32,
+}
+
+macro_rules! try_from {
+    ($ty:ty, $value:ident, $impl: expr) => {
+        impl TryFrom<&$ty> for crate::event::Event {
+            type Error = std::io::Error;
+
+            fn try_from($value: &$ty) -> std::result::Result<Self, Self::Error> {
+                $impl
+            }
+        }
+
+        impl TryFrom<$ty> for crate::event::Event {
+            type Error = std::io::Error;
+
+            fn try_from(value: $ty) -> std::result::Result<Self, Self::Error> {
+                (&value).try_into()
+            }
+        }
+    };
+}
+use try_from;
