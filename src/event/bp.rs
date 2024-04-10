25947e586c0b610e0c9f21b7bd5c067b5039a059
Thaumy <thaumy@outlook.com>
2024-04-10 20:47:54 +0800

    convert from `bp::Len` to bp_len

--- src/event/bp.rs
+++ src/event/bp.rs
@@ -1,3 +1,7 @@
+use std::io::Result;
+
+use crate::ffi::bindings as b;
+
 #[derive(Clone, Debug)]
 pub struct Breakpoint {
     pub ty: Type,
@@ -22,4 +26,20 @@ pub enum Len {
     _6,
     _7,
     _8,
+}
+
+impl Len {
+    pub(crate) const fn as_bp_len(&self) -> Result<u64> {
+        let bp_len = match self {
+            Self::_1 => b::HW_BREAKPOINT_LEN_1,
+            Self::_2 => b::HW_BREAKPOINT_LEN_2,
+            Self::_3 => b::HW_BREAKPOINT_LEN_3,
+            Self::_4 => b::HW_BREAKPOINT_LEN_4,
+            Self::_5 => b::HW_BREAKPOINT_LEN_5,
+            Self::_6 => b::HW_BREAKPOINT_LEN_6,
+            Self::_7 => b::HW_BREAKPOINT_LEN_7,
+            Self::_8 => b::HW_BREAKPOINT_LEN_8,
+        };
+        Ok(bp_len as _)
+    }
 }
