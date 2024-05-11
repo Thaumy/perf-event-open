a49fe68741dc22f81d33a24e852ba97e52756c89
Thaumy <thaumy@outlook.com>
2024-05-11 18:40:12 +0800

    support async iterate AUX area

--- src/sample/auxiliary/iter/mod.rs
+++ src/sample/auxiliary/iter/mod.rs
@@ -1,3 +1,4 @@
+use std::io::Result;
 use std::num::NonZeroUsize;
 
 mod cow;
@@ -13,5 +14,17 @@ impl<'a> Iter<'a> {
 
     pub fn into_cow(self) -> CowIter<'a> {
         self.0
+    }
+
+    pub fn into_async(self) -> Result<AsyncIter<'a>> {
+        Ok(AsyncIter(self.0.into_async()?))
+    }
+}
+
+pub struct AsyncIter<'a>(AsyncCowIter<'a>);
+
+impl AsyncIter<'_> {
+    pub async fn next(&mut self, max_chunk_len: Option<NonZeroUsize>) -> Option<Vec<u8>> {
+        self.0.next(|cc| cc.into_owned(), max_chunk_len).await
     }
 }
