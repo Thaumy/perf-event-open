eb9be319fe4ee36a982110d2fff9d5d297075274
Thaumy <thaumy@outlook.com>
2024-06-25 11:29:34 +0800

    add docs for software events

--- src/event/sw.rs
+++ src/event/sw.rs
@@ -1,25 +1,55 @@
 use super::EventConfig;
 use crate::ffi::bindings as b;
 
+/// Software events provided by the kernel.
 #[derive(Clone, Debug)]
 pub enum Software {
+    /// This reports the CPU clock, a high-resolution per-CPU timer.
     CpuClock,
+    /// This reports a clock count specific to the task that is running (in nanoseconds).
     TaskClock,
 
+    /// The number of page faults.
     PageFault,
+    /// The number of minor page faults. These did not require disk I/O to handle.
     MinorPageFault,
+    /// The number of major page faults. These required disk I/O to handle.
     MajorPageFault,
 
+    /// The number of emulation faults.
+    ///
+    /// The kernel sometimes traps on unimplemented instructions and emulates them for
+    /// user space. This can negatively impact performance.
     EmuFault,
+    /// The number of alignment faults.
+    ///
+    /// These happen when unaligned memory accesses happen; the kernel can handle these but
+    /// it reduces performance. This happens only on some architectures (never on x86).
     AlignFault,
 
+    /// This number of context switches.
     CtxSwitch,
+    /// This counts context switches to a task in a different cgroup.
+    ///
+    /// In other words, if the next task is in the same cgroup, it won't count the switch.
+    ///
     /// Since `linux-5.13`: <https://github.com/torvalds/linux/commit/d0d1dd628527c77db2391ce0293c1ed344b2365f>
     CgroupSwitch,
 
+    /// A placeholder event that counts nothing.
+    ///
+    /// Informational sample record types such as [`Mmap`][crate::sample::record::mmap::Mmap]
+    /// or [`Comm`][crate::sample::record::comm::Comm] must be associated with an active event.
+    /// This dummy event allows gathering such records without requiring a counting event.
     Dummy,
+    /// This is used to generate raw sample data from BPF.
+    ///
+    /// BPF programs can write to this event using `bpf_perf_event_output` helper.
+    ///
     /// Since `linux-4.4`: <https://github.com/torvalds/linux/commit/a43eec304259a6c637f4014a6d4767159b6a3aa3>
     BpfOutput,
+
+    /// The number of times the process has migrated to a new CPU.
     CpuMigration,
 }
 
