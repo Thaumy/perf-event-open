180148a97ec914c6cda597cf5a6d08c97387690e
Thaumy <thaumy@outlook.com>
2024-06-27 15:35:12 +0800

    add docs for event target

--- src/config/target.rs
+++ src/config/target.rs
@@ -3,27 +3,59 @@ use std::os::fd::AsRawFd;
 
 use crate::ffi::bindings as b;
 
+/// Monitor all processes (if [`Proc`] is not set) or all CPUs (if [`Cpu`] is not set).
 #[derive(Clone, Copy, Debug)]
 pub struct All;
 
+/// Which CPU to monitor.
 #[derive(Clone, Copy, Debug)]
 pub struct Cpu(pub u32);
 
 impl Cpu {
+    /// Monitor all CPUs.
+    ///
+    /// This is an alias for [`All`].
     pub const ALL: All = All;
 }
 
+/// Which process (thread) to monitor.
+///
+/// Construct with pid or tid.
+///
+/// `Proc(0)` indicates the current process.
 #[derive(Clone, Copy, Debug)]
 pub struct Proc(pub u32);
 
 impl Proc {
+    /// Monitor all processes.
+    ///
+    /// This is an alias for [`All`].
     pub const ALL: All = All;
+
+    /// Monitor current process.
     pub const CURRENT: Proc = Proc(0);
 }
 
+/// Which cgroup to monitor.
+///
+/// For instance, if the cgroup to monitor is called test, then a file descriptor opened on
+/// `/dev/cgroup/test` (assuming cgroupfs is mounted on `/dev/cgroup`) should be passed.
+///
+/// cgroup monitoring is available only for system-wide events and may therefore require
+/// extra permissions.
 #[derive(Clone, Copy, Debug)]
 pub struct Cgroup<'a>(pub &'a File);
 
+/// Event target, the process (or cgroup) and CPU to monitor.
+///
+/// To create an event target, combine these types in a tuple: [`Proc`] (or [`Cgroup`]), [`Cpu`] and [`All`].
+///
+/// For example, we want to monitor process with pid 12345 on all CPUs: `(Pid(12345), Cpu::ALL)`.
+/// The order of types in the tuples is not senstive because we impl `Into<Target>` for these
+/// swapped tuples, e.g. `(Cpu::ALL, Pid(12345))` has the same semantics as the example above.
+///
+/// This design limits what we can monitor at compile time. For example, the kernel not support
+/// monitoring any process on all CPUs, or a cgroup on all CPUs.
 #[derive(Clone)]
 pub struct Target {
     pub(crate) pid: i32,
