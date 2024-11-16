f0bd520882c0d50f8b0c58cc18ff1efa10f0270c
Thaumy <thaumy@outlook.com>
2024-11-16 09:23:18 +0800

    add `deref_offset` fn

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,1 +1,10 @@
 pub mod bindings;
+
+// Dereferences the pointer and offsets by the size of the
+// pointee type, then returns the dereferenced value.
+#[inline]
+pub unsafe fn deref_offset<T: Copy>(ptr: &mut *const u8) -> T {
+    let val = *(*ptr as *const T);
+    *ptr = ptr.add(size_of::<T>());
+    val
+}
