92d3d2d6862241ab5c79947582e9199f336ee0d4
Thaumy <thaumy@outlook.com>
2025-03-01 18:21:47 +0800

    add docs for comm record

--- src/sample/record/comm.rs
+++ src/sample/record/comm.rs
@@ -4,13 +4,52 @@ use std::mem::align_of;
 use super::{RecordId, SampleType, Task};
 use crate::ffi::{bindings as b, deref_offset};
 
+/// Process name (comm) has been changed.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::ffi::CStr;
+///
+/// use perf_event_open::config::{Cpu, Opts, Proc};
+/// use perf_event_open::count::Counter;
+/// use perf_event_open::event::sw::Software;
+/// # use perf_event_open::sample::record::Record;
+///
+/// let event = Software::Dummy;
+/// let target = (Proc::CURRENT, Cpu::ALL);
+///
+/// let mut opts = Opts::default();
+/// opts.extra_record.comm = true;
+///
+/// let counter = Counter::new(event, target, opts).unwrap();
+/// let sampler = counter.sampler(5).unwrap();
+///
+/// counter.enable().unwrap();
+///
+/// // Change the process name to "foo" to trigger the `Comm` record.
+/// let name = CStr::from_bytes_with_nul(b"foo\0").unwrap();
+/// unsafe { libc::prctl(libc::PR_SET_NAME, name.as_ptr()) };
+///
+/// # let mut vec = vec![];
+/// let mut iter = sampler.iter();
+/// while let Some(it) = iter.next() {
+///     println!("{:-?}", it);
+///     # vec.push(it);
+/// }
+/// # assert!(vec.iter().any(|(_, it)| matches!(it, Record::Comm(_))));
+/// ```
 #[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Comm {
+    /// Record IDs.
     pub record_id: Option<RecordId>,
 
+    /// Whether comm was changed by `execve`.
     pub by_execve: bool,
+    /// Task info.
     pub task: Task,
+    /// New comm.
     pub comm: CString,
 }
 
