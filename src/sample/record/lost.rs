b0d7beea6e4a4fc6c61f1462adc6c291265b7254
Thaumy <thaumy@outlook.com>
2025-03-15 23:30:37 +0800

    add docs for `LostRecords`

--- src/sample/record/lost.rs
+++ src/sample/record/lost.rs
@@ -13,12 +13,59 @@ use crate::ffi::deref_offset;
 // The same applies to `perf_read`:
 // https://github.com/torvalds/linux/blob/v6.13/kernel/events/core.c#L5764
 // https://github.com/torvalds/linux/blob/v6.13/kernel/events/core.c#L7373
+/// Some records have been lost.
+///
+/// # Examples
+///
+/// ```rust
+/// # #[cfg(not(feature = "linux-6.0"))]
+/// # return;
+/// #
+/// # tokio_test::block_on(async {
+/// use std::thread;
+/// use std::time::Duration;
+///
+/// use perf_event_open::config::{Cpu, Opts, Proc, SampleOn, WakeUpOn};
+/// use perf_event_open::count::Counter;
+/// use perf_event_open::event::sw::Software;
+/// use perf_event_open::sample::record::Record;
+///
+/// let event = Software::TaskClock;
+/// let target = (Proc::ALL, Cpu(0));
+///
+/// let mut opts = Opts::default();
+/// opts.stat_format.lost_records = true;
+/// opts.wake_up.on = WakeUpOn::Samples(1);
+/// opts.sample_on = SampleOn::Count(1_000_000); // 1ms
+///
+/// let counter = Counter::new(event, target, opts).unwrap();
+/// let sampler = counter.sampler(5).unwrap();
+///
+/// counter.enable().unwrap();
+/// // Pause the ring-buffer output, discarded samples are considered lost.
+/// sampler.pause().unwrap();
+/// thread::sleep(Duration::from_millis(10));
+/// sampler.resume().unwrap();
+///
+/// let mut iter = sampler.iter().into_async().unwrap();
+/// while let Some((_, r)) = iter.next().await {
+///     if let Record::LostRecords(l) = r {
+///         println!("{:-?}", l);
+///         # assert!(l.lost_records > 0);
+///         break;
+///     }
+/// }
+/// # });
+/// ```
 #[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct LostRecords {
+    /// Record IDs.
     pub record_id: Option<RecordId>,
 
+    /// Event ID.
     pub id: u64,
+    /// The number of lost records.
     pub lost_records: u64,
 }
 
