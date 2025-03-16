285b7b22caf18f840ddac33e93edf1450a205910
Thaumy <thaumy@outlook.com>
2025-03-16 20:08:22 +0800

    add docs for cgroup record

--- src/sample/record/cgroup.rs
+++ src/sample/record/cgroup.rs
@@ -2,13 +2,58 @@ use std::ffi::CString;
 
 use super::RecordId;
 
+/// Process created a new cgroup.
+///
+/// # Examples
+///
+/// Running this example may require root privileges.
+///
+/// ```rust, no_run
+/// use perf_event_open::config::{Cpu, Opts, Proc};
+/// use perf_event_open::count::Counter;
+/// use perf_event_open::event::sw::Software;
+///
+/// let event = Software::Dummy;
+/// let target = (Proc::CURRENT, Cpu::ALL);
+/// let mut opts = Opts::default();
+/// opts.extra_record.cgroup = true;
+///
+/// let counter = Counter::new(event, target, opts).unwrap();
+/// let sampler = counter.sampler(5).unwrap();
+///
+/// counter.enable().unwrap();
+/// let path = format!("/sys/fs/cgroup/{}", uuid::Uuid::new_v4());
+/// std::fs::create_dir(&path).unwrap();
+/// std::fs::remove_dir(&path).unwrap();
+///
+/// # let mut cgroup_record = false;
+/// for it in sampler.iter() {
+///     println!("{:-?}", it);
+///     # use perf_event_open::sample::record::Record;
+///     # if let Record::Cgroup(c) = &it.1 {
+///     #     use std::path::Path;
+///     #     assert_eq!(
+///     #         Path::new(&c.path.to_string_lossy().to_string())
+///     #             .file_name()
+///     #             .unwrap(),
+///     #         Path::new(&path).file_name().unwrap()
+///     #     );
+///     #     cgroup_record = true;
+///     # }
+/// }
+/// # assert!(cgroup_record);
+/// ```
+///
 /// Since `linux-5.7`: <https://github.com/torvalds/linux/commit/96aaab686505c449e24d76e76507290dcc30e008>
 #[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Cgroup {
+    /// Record IDs.
     pub record_id: Option<RecordId>,
 
+    /// Cgroup ID.
     pub id: u64,
+    /// Cgroup path from the root cgroup.
     pub path: CString,
 }
 
