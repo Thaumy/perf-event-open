44c849b8b744edaf1f26f637adb2ceb132ed1fd4
Thaumy Cheng <thaumy.love@gmail.com>
2025-12-08 22:25:16 +0800

    upgrade rustfmt to nightly 2025-12-08

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkgs.url = "github:NixOS/nixpkgs/eb6cf22e8ee7d1307705d8cac7c1f81b8135c2ca"; # 25-4-3
     rust-overlay = {
-      url = "github:oxalica/rust-overlay/9d00c6b69408dd40d067603012938d9fbe95cfcd"; # 24-4-6
+      url = "github:oxalica/rust-overlay/7ed7e8c74be95906275805db68201e74e9904f07"; # 25-12-8
       inputs.nixpkgs.follows = "pkgs";
     };
     flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b"; # 24-11-14
@@ -29,7 +29,7 @@
         name = "perf-event-open";
 
         # Use nightly fmt for better style
-        RUSTFMT = "${rust-toolchain "nightly" "2025-04-03"}/bin/rustfmt";
+        RUSTFMT = "${rust-toolchain "nightly" "2025-12-08"}/bin/rustfmt";
 
         nativeBuildInputs = [
           (rust-toolchain "stable" "1.80.0")
