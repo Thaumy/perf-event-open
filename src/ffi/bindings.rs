49969cf0749e1b6244a988397406cc675ba64dc0
Thaumy Cheng <thaumy.love@gmail.com>
2026-07-12 08:13:11 +0800

    simplify binding selection macro

--- src/ffi/bindings.rs
+++ src/ffi/bindings.rs
@@ -1,7 +1,7 @@
 #![allow(warnings)]
 
-macro_rules! select {
-    ($([$($since:literal)? .. $($before:literal)?],)+) => {
+macro_rules! select [
+    ($($($since:literal)? .. $($before:literal)?,)+) => {
         $(select!($($since)? .. $($before)?);)+
     };
     // internal switches
@@ -18,42 +18,43 @@ macro_rules! select {
         #[cfg(not(feature = $before))]
         include!(concat!("bindings/.", $before, ".rs"));
     };
-}
+];
 
-select! {
-    ["linux-7.0" ..            ],
-    ["linux-6.19".."linux-7.0" ],
-    ["linux-6.13".."linux-6.19"],
-    ["linux-6.11".."linux-6.13"],
-    ["linux-6.8" .."linux-6.11"],
-    ["linux-6.6" .."linux-6.8" ],
-    ["linux-6.3" .."linux-6.6" ],
-    ["linux-6.1" .."linux-6.3" ],
-    ["linux-6.0" .."linux-6.1" ],
-    ["linux-5.18".."linux-6.0" ],
-    ["linux-5.17".."linux-5.18"],
-    ["linux-5.16".."linux-5.17"],
-    ["linux-5.13".."linux-5.16"],
-    ["linux-5.12".."linux-5.13"],
-    ["linux-5.11".."linux-5.12"],
-    ["linux-5.9" .."linux-5.11"],
-    ["linux-5.7" .."linux-5.9" ],
-    ["linux-5.5" .."linux-5.7" ],
-    ["linux-5.4" .."linux-5.5" ],
-    ["linux-5.1" .."linux-5.4" ],
-    ["linux-4.17".."linux-5.1" ],
-    ["linux-4.16".."linux-4.17"],
-    ["linux-4.15".."linux-4.16"],
-    ["linux-4.14".."linux-4.15"],
-    ["linux-4.12".."linux-4.14"],
-    ["linux-4.10".."linux-4.12"],
-    ["linux-4.8" .."linux-4.10"],
-    ["linux-4.7" .."linux-4.8" ],
-    ["linux-4.5" .."linux-4.7" ],
-    ["linux-4.4" .."linux-4.5" ],
-    ["linux-4.3" .."linux-4.4" ],
-    ["linux-4.2" .."linux-4.3" ],
-    ["linux-4.1" .."linux-4.2" ],
-    ["linux-4.0" .."linux-4.1" ],
-    [            .."linux-4.0" ],
-}
+#[rustfmt::skip]
+select! [
+    "linux-7.0" ..            ,
+    "linux-6.19".."linux-7.0" ,
+    "linux-6.13".."linux-6.19",
+    "linux-6.11".."linux-6.13",
+    "linux-6.8" .."linux-6.11",
+    "linux-6.6" .."linux-6.8" ,
+    "linux-6.3" .."linux-6.6" ,
+    "linux-6.1" .."linux-6.3" ,
+    "linux-6.0" .."linux-6.1" ,
+    "linux-5.18".."linux-6.0" ,
+    "linux-5.17".."linux-5.18",
+    "linux-5.16".."linux-5.17",
+    "linux-5.13".."linux-5.16",
+    "linux-5.12".."linux-5.13",
+    "linux-5.11".."linux-5.12",
+    "linux-5.9" .."linux-5.11",
+    "linux-5.7" .."linux-5.9" ,
+    "linux-5.5" .."linux-5.7" ,
+    "linux-5.4" .."linux-5.5" ,
+    "linux-5.1" .."linux-5.4" ,
+    "linux-4.17".."linux-5.1" ,
+    "linux-4.16".."linux-4.17",
+    "linux-4.15".."linux-4.16",
+    "linux-4.14".."linux-4.15",
+    "linux-4.12".."linux-4.14",
+    "linux-4.10".."linux-4.12",
+    "linux-4.8" .."linux-4.10",
+    "linux-4.7" .."linux-4.8" ,
+    "linux-4.5" .."linux-4.7" ,
+    "linux-4.4" .."linux-4.5" ,
+    "linux-4.3" .."linux-4.4" ,
+    "linux-4.2" .."linux-4.3" ,
+    "linux-4.1" .."linux-4.2" ,
+    "linux-4.0" .."linux-4.1" ,
+                .."linux-4.0" ,
+];
